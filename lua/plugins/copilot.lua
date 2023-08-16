06f583167000fa6357f8026435a5eafd86aacca6
中野智也 <tomo1999724@gmail.com>
2023-08-16 11:50:54 +0900

    :wrench: Update copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,7 +5,8 @@ return {
     config = function()
       vim.g.copilot_no_tab_map = true
       vim.g.copilot_assume_mapped = true
-      vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")',
+        { noremap = true, silent = true, expr = true, replace_keycodes = false })
     end,
   },
 }
