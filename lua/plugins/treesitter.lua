5ddb592e4f2276dd7724fa1b660f53241bc02ee5
tomoyanakano <tomo1999724@gmail.com>
2023-12-12 13:09:19 +0900

    :wrench: Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,59 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = { "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring" },
-  event = "User AstroFile",
-  cmd = {
-    "TSBufDisable",
-    "TSBufEnable",
-    "TSBufToggle",
-    "TSDisable",
-    "TSEnable",
-    "TSToggle",
-    "TSInstall",
-    "TSInstallInfo",
-    "TSInstallSync",
-    "TSModuleInfo",
-    "TSUninstall",
-    "TSUpdate",
-    "TSUpdateSync",
-  },
-  build = ":TSUpdate",
-  opts = {
-    highlight = {
-      enable = true,
-      disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "windwp/nvim-ts-autotag", "JoosepAlviste/nvim-ts-context-commentstring" },
+    event = "User AstroFile",
+    cmd = {
+      "TSBufDisable",
+      "TSBufEnable",
+      "TSBufToggle",
+      "TSDisable",
+      "TSEnable",
+      "TSToggle",
+      "TSInstall",
+      "TSInstallInfo",
+      "TSInstallSync",
+      "TSModuleInfo",
+      "TSUninstall",
+      "TSUpdate",
+      "TSUpdateSync",
     },
-    incremental_selection = { enable = true },
-    indent = { enable = true },
-    autotag = {
-      enable = true,
+    build = ":TSUpdate",
+    opts = {
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+        disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
+      },
+      incremental_selection = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
     },
-    context_commentstring = { enable = true, enable_autocmd = false },
+    config = function()
+      local parser_configs = require "nvim-treesitter.parsers".get_parser_configs()
+      parser_configs.liquid = {
+        install_info = {
+          url = "https://github.com/Shopify/tree-sitter-liquid.git",
+          files = { "src/parser.c" },
+          branch = "main",
+          generate_requires_npm = false,
+          requires_generate_from_grammar = false,
+        },
+        filetype = "liquid",
+      }
+    end
   },
-  config = require "plugins.configs.nvim-treesitter",
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      local ts_autotag = require('nvim-ts-autotag')
+      ts_autotag.setup({
+        filetypes = { "html", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue", "jsx", "tsx",
+          "liquid", "lua" },
+      })
+    end,
+    lazy = true,
+    event = "VeryLazy"
+  },
 }
